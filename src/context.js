4aaa2eaff8046884cfea8461c88874582087c1b5
Erick Rodriguez <erickrod54@gmail.com>
2022-11-07 12:19:45 -0500

    interview-prep-app - version 1 - context js - Features: Importing and providing data collections from the context

--- src/context.js
+++ src/context.js
@@ -1,15 +1,27 @@
 import React, { useContext } from "react";
-import { nemo } from "./data";
+import { everyoneCharacter, largeArray, nemo } from "./data";
 
+/**interview-prep-app - version 1 - context js - 
+ * Features: 
+ * 
+ *     --> Importing and providing 'nemo', 
+ *         'everyoneCharacter' and 'largeArray' 
+ *          collections to use in 'O notation'.
+ * 
+ * Note: pending to migrate functionalities from
+ * local components
+ */
 
 export const AppContext = React.createContext()
 
 
 export const AppProvider = ({ children }) => {
-
+    
     return(
         <AppContext.Provider value={{
-            nemo,            
+            nemo,
+            everyoneCharacter,
+            largeArray            
             }}>
             {children}
         </AppContext.Provider>
