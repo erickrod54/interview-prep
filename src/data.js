a3066e7a00301702797b6043eee6137fb0eaba4e
Erick Rodriguez <erickrod54@gmail.com>
2022-11-07 12:18:26 -0500

    interview-prep-app - version 1 - data js - Features: Building data nemo, everyoneCharacter and LargeArray collections to use in BigOnotation

--- src/data.js
+++ src/data.js
@@ -1,2 +1,28 @@
+/**interview-prep-app - version 1 - data js - 
+ * Features: 
+ * 
+ *     --> Building data 'nemo', 'everyoneCharacter'
+ *         and 'largeArray' collections to use in
+ *        'O notation'.
+ * 
+ * Note: this data file is going to add more data
+ * to integrate with the exercises
+ */
+
 export const nemo = ['nemo'];
 
+
+export const everyoneCharacter = [
+ 'dory',
+ 'bruce',
+ 'marlin',
+ 'nemo',
+ 'gill',
+ 'bloat',
+ 'nigel',
+ 'squirt',
+ 'darla',
+ 'hank'
+]
+
+export const largeArray = new Array(1000).fill('nemo')
