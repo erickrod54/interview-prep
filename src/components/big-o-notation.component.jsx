6702047a26e445d11c14492c9b5be05c8b011fba
Erick Rodriguez <erickrod54@gmail.com>
2022-11-07 12:21:35 -0500

    interview-prep-app - version 1 - BigOnotation - Features: Building BigOnotation, Building states and hanlder to toggle functionalities, Building the return

--- src/components/big-o-notation.component.jsx
+++ src/components/big-o-notation.component.jsx
@@ -1,13 +1,52 @@
-import React from "react";
+import React, { useState } from "react";
 import styled from "styled-components";
 import { useAppContext } from "../context";
 
+/**interview-prep-app - version 1 - BigOnotation - 
+ * Features: 
+ * 
+ *     --> Building 'BigOnotation'.
+ * 
+ *     --> Building states and handlers to toggle 
+ *         the functionalities.
+ * 
+ *     --> Building the return.
+ * 
+ * Note: pending to migrate functionalities and states 
+ * to the context and provide them from there
+ */
+
 const BigOnotation = () => {
 
-    const { nemo } = useAppContext()
+    const [ showNemo, setShowNemo ] = useState(false)
+    const [ showEveryone, setShowEveryone ] = useState(false)
+    const [ showLarge, setShowLarge ] = useState(false)
+    const [showNemoWithTime, setShowNemoWithTime ] = useState(false)
 
-    console.log('context works ==>', nemo)
-    
+    const { nemo, everyoneCharacter, largeArray } = useAppContext()
+
+    const handleFindNemo = () => {
+        setShowNemo(!showNemo)
+        findNemo(nemo)
+    }
+
+    const handleFindNemoWithTime = () => {
+        setShowNemoWithTime(!showNemoWithTime)
+        
+        findNemoWithTime(nemo)
+    }
+
+    const handleFindNemoEveryone = () => {
+        setShowEveryone(!showEveryone)
+        
+        findNemoWithTime(everyoneCharacter)
+    }
+
+    const handleFindNemoLarge = () => {
+        setShowLarge(!showLarge)
+     
+        findNemoWithTime(largeArray)
+    }
 
     const findNemo = (array) => {
         for( let i = 0; i <  array.length; i++){
@@ -18,6 +57,20 @@ const BigOnotation = () => {
             }
         }
     }
+
+    /**this is to measure the performance */
+    const findNemoWithTime = (array) => {
+        let t0 = performance.now()
+        for( let i = 0; i <  array.length; i++){
+            if (array[i] === 'nemo') {
+                return(
+                    console.log("Found Nemo measuring time!!!")
+                    )
+                }
+            }
+            let t1 = performance.now()
+            console.log('call to find nemo took ==>', + (t1-t0) + ' miliseconds')
+        }
 
     return(
         <Wrapper>
@@ -52,6 +105,7 @@ const BigOnotation = () => {
                             <p>{`console.log("Found Nemo!!!")`}</p>
                             <p>{`)`}</p>
                             <p>{`}`}</p>
+                            <p>{`let t1 = performance.now()`}</p>
                         </section>
                     </li>
                 </ul>
@@ -61,9 +115,84 @@ const BigOnotation = () => {
                     'nemo', the how efficient is the way that i do it ?
                      is evaluated with big-o notation ( Big O Asymptotic Analisys)
                 </p>
-                <button onClick={() => findNemo(nemo)}>
+                <button onClick={handleFindNemo}>
                     click to find nemo
                 </button>
+
+                <section className="nemo-section">
+                { showNemo ? 
+                    <h2>Nemo Found!!</h2>
+                    :
+                    null    
+                }
+                </section>
+                
+                <p>after this, i made it, nemo was found, but the question is how ?,
+                    comes to mind.. how i measure based on 'readability' or 
+                    'scalability', i can start setting timers and stressing my
+                    code under the worst conditions (once i click,
+                    i can check changes in the javaConsole):  
+                </p>
+                <section className="code-block">
+                            <p>{`let t0 = performance.now()`}</p>
+                            <p>{`for( let i = 0; i <  array.length; i++){`}</p>
+                            <p>{`if (array[i] === 'nemo') {`}</p>
+                            <p>{`return(`}</p>
+                            <p>{`console.log("Found Nemo measuring time!!!")`}</p>
+                            <p>{`)`}</p>
+                            <p>{`}`}</p>
+                            <p>{`console.log('call to find nemo took ==>', + (t1-t0) + ' miliseconds')`}</p>        
+                        </section>
+                <p>here i have three conditions to test code under stress
+                  and check how fast the code is (this depends also,
+                  on how fast is the computer where the code runs -this
+                  test can be done in developer enviroment or VM emulating
+                  production-), the first one is just with nemo:
+                </p>
+                <button onClick={ () => handleFindNemoWithTime()}>
+                    click to find nemo measuring with 'nemo'
+                </button>
+
+                <section className="nemo-section">
+                { showNemoWithTime ? 
+                    <h5>Nemo Found between 1 time measuring time!!</h5>
+                    :
+                    null    
+                }
+                </section>
+
+                <p>the second one is using the array 'everyone'
+                    that has 10 charaters, so the code will stress
+                    running 10 times to find 'nemo':
+                </p>
+
+                <button onClick={() => handleFindNemoEveryone()}>
+                    click to find nemo measuring with 'everyone'
+                </button>
+
+                <section className="nemo-section">
+                { showEveryone ? 
+                    <h5>Nemo Found between 10 iterations!!</h5>
+                    :
+                    null    
+                }
+                </section>
+                
+                <p>the third one is using the array 'large'
+                    that has 1000 'nemo' fills, so the code will 
+                    stress running 1000 times to find 'nemo':
+                </p>
+                <button onClick={() => handleFindNemoLarge()}>
+                    click to find nemo measuring with 'large'
+                </button>
+
+                <section className="nemo-section">
+                { showLarge ? 
+                    <h5>Nemo Found betwwen 1000 nemo refils!!</h5>
+                    :
+                    null    
+                }
+                </section>
             </div>
         </Wrapper>
     )
@@ -79,6 +208,16 @@ const Wrapper = styled.div`
         display: flex;
         justify-content: center;
         align-content: center;
+    }
+
+    .nemo-section{
+        border: 1px solid black;
+        display: flex;
+        justify-content: center;
+        align-items: center;
+        margin: 2rem;
+        width: 90%;
+        height: 5rem;
     }
 `
 
