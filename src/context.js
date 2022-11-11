3ef0379326c7a0a04d683bd468d33654ac09976a
Erick Rodriguez <erickrod54@gmail.com>
2022-11-11 04:20:20 -0500

    interview-prep-app - version 2 - context js - Features: Importing and providing graphsData

--- src/context.js
+++ src/context.js
@@ -1,12 +1,10 @@
 import React, { useContext } from "react";
-import { everyoneCharacter, largeArray, nemo } from "./data";
+import { everyoneCharacter, graphsData, largeArray, nemo } from "./data";
 
-/**interview-prep-app - version 1 - context js - 
+/**interview-prep-app - version 2 - context js - 
  * Features: 
  * 
- *     --> Importing and providing 'nemo', 
- *         'everyoneCharacter' and 'largeArray' 
- *          collections to use in 'O notation'.
+ *     --> Importing and providing 'graphsData'.
  * 
  * Note: pending to migrate functionalities from
  * local components
@@ -21,7 +19,8 @@ export const AppProvider = ({ children }) => {
         <AppContext.Provider value={{
             nemo,
             everyoneCharacter,
-            largeArray            
+            largeArray,
+            graphsData           
             }}>
             {children}
         </AppContext.Provider>
