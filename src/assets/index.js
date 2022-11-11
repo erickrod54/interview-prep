3dfd991853edf93fd664caea3f0efeebc51a0ce0
Erick Rodriguez <erickrod54@gmail.com>
2022-11-11 04:17:18 -0500

    interview-prep-app - version 2 - assets > index - Features: Importing and exporting assets from the index

---
+++ src/assets/index.js
@@ -0,0 +1,14 @@
+import bigo1stgraph from './big-o-graph1.png';
+import bigo2ndgraph from './big-o-graph2.png';
+
+/**interview-prep-app - version 2 - assets > index - 
+ * Features: 
+ * 
+ *     --> Importing and exporting assets from
+ *          the index.
+ * 
+ * Note: depending on how big will be this component
+ * i'll make it's own context
+ */
+
+export { bigo1stgraph, bigo2ndgraph };
