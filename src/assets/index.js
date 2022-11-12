c053dedbc77b797c0ad469b5f2b43b55f4e0d56b
Erick Rodriguez <erickrod54@gmail.com>
2022-11-12 06:35:56 -0500

    interview-prep-app version 3 - assets > index - Features: Importing and exporting assets related with big o complexity constant time to the index

--- src/assets/index.js
+++ src/assets/index.js
@@ -1,14 +1,18 @@
 import bigo1stgraph from './big-o-graph1.png';
 import bigo2ndgraph from './big-o-graph2.png';
+import bigo3const from './big-o-constant-time-3-op.png';
+import bigoconst from './big-o-constant-time.png';
+import bigochart from './big-o-complexity-chart.png'
 
-/**interview-prep-app - version 2 - assets > index - 
+/**interview-prep-app - version 3 - assets > index - 
  * Features: 
  * 
- *     --> Importing and exporting assets from
- *          the index.
+ *     --> Importing and exporting assets related
+ *          with 'big o complexity constant time' 
+ *          to the index.
  * 
  * Note: depending on how big will be this component
  * i'll make it's own context
  */
 
-export { bigo1stgraph, bigo2ndgraph };
+export { bigo1stgraph, bigo2ndgraph, bigoconst, bigo3const, bigochart };
