e7ef880333731b7b108d0a629aeab054a056ebcc
Erick Rodriguez <erickrod54@gmail.com>
2022-11-18 18:06:39 -0500

    interview-prep-app - version 4 - HomeInterviewPrep - version 4 - Features: Destructuring data from the context, Making url dinamic, Giving some style

--- src/pages/home.page.jsx
+++ src/pages/home.page.jsx
@@ -1,14 +1,36 @@
 import React from "react";
 import { Link } from 'react-router-dom'
 import styled from "styled-components";
+import { useAppContext } from "../context";
+
+/**interview-prep-app - version 4 - 
+ * HomePageInterviewPrep - Features: 
+ * 
+ *     --> Destructuring data from the
+ *          context.
+ * 
+ *     --> Making url dinamic.
+ * 
+ *     --> Giving some style 
+ * 
+ * Note: to generate url's dinamicly
+ */
 
 const HomePageInterviewPrep = () => {
 
+    const { linksData } = useAppContext()
+    console.log(linksData)
+
     return(
         <Wrapper>
-            <h2 className="main-title">The interview prep</h2>
             <div className="topics-container">
-                <Link to='/big-o-notation'>Big O notation</Link>
+                {linksData.map((item) => {
+                    const { id, url, name } = item;
+
+                    return(
+                        <Link key={id} to={url}>{name}</Link>
+                    )
+                })}
 
             </div>
         </Wrapper>
@@ -17,7 +39,16 @@ const HomePageInterviewPrep = () => {
 
 
 const Wrapper = styled.div`
-    
+    .topics-container{
+        display: flex;
+        justify-content: space-evenly;
+
+        a{
+            text-transform: capitalize;
+            text-decoration: 2px underline orange;
+            color: deepskyblue;
+        }
+    }
 `
 
 export default HomePageInterviewPrep;
