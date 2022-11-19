3c419b9194b3c20a1f5c5502d7aaae7388475853
Erick Rodriguez <erickrod54@gmail.com>
2022-11-19 18:21:03 -0500

    interview-prep-app - version 5 - components > index - Features: Adding BigORuleBook and Nav to the index

--- src/components/index.components.jsx
+++ src/components/index.components.jsx
@@ -1,3 +1,15 @@
 import BigOnotation from "./big-o-notation.component";
+import BigORuleBook from "./big-o-notation.rules.component";
+import Nav from "./nav.component";
 
-export { BigOnotation }
+/**interview-prep-app - version 5 - 
+ * components > index - Features: 
+ * 
+ *     --> Adding 'BigORuleBook', and 'Nav' 
+ *          to the index. 
+ * 
+ * Note: the first assets added to 'graphsData'
+ * are for big notation app
+ */
+
+export { BigOnotation, BigORuleBook, Nav }
