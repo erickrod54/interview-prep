ae44db90f368c2ee963ab267cb4d2037dd3470cf
Erick Rodriguez <erickrod54@gmail.com>
2022-11-20 20:42:36 -0500

    interview-prep-app - version 7 - App js - Features: Adding SpaceComplexity and building a route for it

--- src/App.js
+++ src/App.js
@@ -1,16 +1,12 @@
 import React from "react";
 import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
-import { BigOnotation, BigORuleBook, Nav } from "./components/index.components";
+import { BigOnotation, BigORuleBook, Nav, SpaceComplexity } from "./components/index.components";
 import { HomePageInterviewPrep } from "./pages/index.pages";
 
-/**interview-prep-app - version 4 - App js - 
+/**interview-prep-app - version 7 - App js - 
  * Features: 
  * 
- *     --> Importing and placing 'Nav'
- *          to provide common navigation.
- * 
- *     --> Importing 'BigORuleBook' and 
- *          building a route for it.  
+ *     --> Adding 'SpaceComplexity'.  
  * 
  * Note: the first assets added to 'graphsData'
  * are for big notation app
@@ -24,6 +20,7 @@ function App() {
           <Route path='/' element={<HomePageInterviewPrep />}/>
           <Route path='/big-o-notation' element={<BigOnotation />}/>
           <Route path='/big-o-rule-book' element={<BigORuleBook />}/>
+          <Route path='/space-complexity' element={<SpaceComplexity />}/>
       </Routes>
     </Router>
   );
