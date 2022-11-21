3d0251dd9877295f8a7def33e8ee57b1a3870372
Erick Rodriguez <erickrod54@gmail.com>
2022-11-21 08:36:11 -0500

    interview-prep-app - version 8 - components > index - Features: Adding SpaceComplexity to the index

--- src/components/index.components.jsx
+++ src/components/index.components.jsx
@@ -1,15 +1,16 @@
 import BigOnotation from "./big-o-notation.component";
 import BigORuleBook from "./big-o-notation.rules.component";
 import Nav from "./nav.component";
+import SpaceComplexity from "./space.complexity.component";
 
-/**interview-prep-app - version 5 - 
+/**interview-prep-app - version 8 - 
  * components > index - Features: 
  * 
- *     --> Adding 'BigORuleBook', and 'Nav' 
+ *     --> Adding 'SpaceComplexity' 
  *          to the index. 
  * 
  * Note: the first assets added to 'graphsData'
  * are for big notation app
  */
 
-export { BigOnotation, BigORuleBook, Nav }
+export { BigOnotation, BigORuleBook, Nav, SpaceComplexity }
