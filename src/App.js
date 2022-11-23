e81b7ab81fb2543a35fe0493df9dd1c64d25d28d
Erick Rodriguez <erickrod54@gmail.com>
2022-11-23 13:09:19 -0500

    interview-prep-app - version 9 - App js - Features: Adding HowToSolveProblems and building a route for it

--- src/App.js
+++ src/App.js
@@ -1,12 +1,12 @@
 import React from "react";
 import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
-import { BigOnotation, BigORuleBook, Nav, SpaceComplexity } from "./components/index.components";
+import { BigOnotation, BigORuleBook, HowToSolveProblems, Nav, SpaceComplexity } from "./components/index.components";
 import { HomePageInterviewPrep } from "./pages/index.pages";
 
-/**interview-prep-app - version 7 - App js - 
+/**interview-prep-app - version 9 - App js - 
  * Features: 
  * 
- *     --> Adding 'SpaceComplexity'.  
+ *     --> Adding 'HowToSolveProblems'.  
  * 
  * Note: the first assets added to 'graphsData'
  * are for big notation app
@@ -21,6 +21,7 @@ function App() {
           <Route path='/big-o-notation' element={<BigOnotation />}/>
           <Route path='/big-o-rule-book' element={<BigORuleBook />}/>
           <Route path='/space-complexity' element={<SpaceComplexity />}/>
+          <Route path='/how-to-solve-problems' element={<HowToSolveProblems />}/>
       </Routes>
     </Router>
   );
