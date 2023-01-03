470ff26a9c2e315504f54769fff155b735deab5f
Erick Rodriguez <erickrod54@gmail.com>
2023-01-02 21:09:57 -0500

    interview-prep-app - version 13 - components > index - Features: Adding ArrayImplementations to the index

--- src/components/index.components.jsx
+++ src/components/index.components.jsx
@@ -1,17 +1,18 @@
+import ArraysImplementations from "./arrays.implementations";
 import BigOnotation from "./big-o-notation.component";
 import BigORuleBook from "./big-o-notation.rules.component";
 import HowToSolveProblems from "./how-to-solve-problems.component";
 import Nav from "./nav.component";
 import SpaceComplexity from "./space.complexity.component";
 
-/**interview-prep-app - version 9 - 
+/**interview-prep-app - version 13 - 
  * components > index - Features: 
  * 
- *     --> Adding 'HowToSolveProblems' 
+ *     --> Adding 'ArraysImplementations' 
  *          to the index. 
  * 
  * Note: the first assets added to 'graphsData'
  * are for big notation app
  */
 
-export { BigOnotation, BigORuleBook, Nav, SpaceComplexity, HowToSolveProblems }
+export { BigOnotation, BigORuleBook, Nav, SpaceComplexity, HowToSolveProblems, ArraysImplementations }
