cea83acc0fab2679b896ddbdc3d5ddc3f794498e
Erick Rodriguez <erickrod54@gmail.com>
2023-01-02 21:11:10 -0500

    interview-prep-app - version 13 - context js - Features: fixing CommonElement and CommonElement2

--- src/context.js
+++ src/context.js
@@ -1,56 +1,67 @@
-import React, { useContext, useState } from "react";
+import React, { useContext } from "react";
 import { array1, array2, array3, array4, boxes, boxes1, boxesletters, everyoneCharacter, graphsData, largeArray, linksData, n, nemo, numbers } from "./data";
 
-/**interview-prep-app - version 12 - context js - 
+/**interview-prep-app - version 13 - context js - 
  * Features: 
  * 
- *     --> work in progress for 'CommonElement2'
+ *     --> fixing 'CommonElement' and 'CommonElement2'
  * 
  * Note: pending to migrate functionalities from
  * local components
  */
 
+
 export const AppContext = React.createContext()
 
 export const AppProvider = ({ children }) => {
-
+    
     /**--- logic for HowToSolveProblems Component start--*/
-    const [ common, setCommon ] = useState(false)
-    const [ common1, setCommon1 ] = useState(true)
+    
     
     
     const CommonElement = (arr1, arr2 ) => {
-        for (let i = 0; i < arr1.length; i++) {
-           for (let j = 0; j < arr2.length; j++) {
+        /**big o (a * b) because is nested*/
+        for(let i=0; i < arr1.length; i++){
+            for(let j=0; j < arr2.length; j++){
                 if (arr1[i] === arr2[j]) {
-                   setCommon(!common)
-                   return console.log('common element found ==>', true)
+                    return console.log('(a * b) - if i get here is because arra3 and array4 have at least one equal ==>',true)
                 }
             }
-            setCommon1(!common1)
-            return console.log('not common element found ==>',false)
         }
+        return console.log('(a * b) - array1 and array2 are not equal and is ==>',false)
     }
-
-    const CommonElement2 = (arr1, arr2 ) => {
+    
+    const CommonElement2 = (arr3, arr4 ) => {
+        /**big o (a + b) because they are comparing separately*/
+        
         let map = {};
-
-        for (let i = 0; i < arr1.length; i ++){
-            if (!map[array1[i]]) {
-                const item = array1[i];
-                map[item] = true
+        
+        for(let i=0; i < arr3.length; i++){
+            if (!map[arr3[i]]) {
+                const item = arr3[i]
+                map[item] = true;
             }
         }
-        console.log('value from CommonElement2 ==> ',map)
-
-        for (let j = 0; j < arr2.length; j ++){
-            if (map[array2[j]]) {
-                return console.log(true)
+        //console.log('this is array 3 ==>', map)
+        
+        for(let j=0; j < arr4.length; j++){
+            if (map[arr4[j]]) {
+                return console.log('(a + b) - if i get here is because arra3 and array4 have at least one equal ==>',true)
             }
         }
-        return console.log(false)
+        return console.log('(a + b) - array1 and array2 are not equal and is ==>',false)
     }
-
+    
+    const CommonElement3 = (arr5, arr6 ) => {
+        return console.log('(a + b) es6 method - array1 and array2 are not equal and is ==>',arr5.some(item => arr6.includes(item)))
+        
+    }
+    
+    const CommonElement4 = (arr7, arr8 ) => {
+        return console.log('(a + b) es6 - if i get here is because arra3 and array4 have at least one equal ==>',arr7.some(item => arr8.includes(item)))
+    }
+    
+    
     /**--- logic for HowToSolveProblems Component end--*/
     
     return(
@@ -69,11 +80,11 @@ export const AppProvider = ({ children }) => {
             array2, 
             array3, 
             array4,
-            common,
-            common1,
             CommonElement,
-            CommonElement2
-            }}>
+            CommonElement2,
+            CommonElement3,
+            CommonElement4,
+        }}>
             {children}
         </AppContext.Provider>
     )
