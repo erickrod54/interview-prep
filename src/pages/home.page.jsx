4343a33b97f37573d4a7f0ea77f1dd314d46f4b0
Erick Rodriguez <erickrod54@gmail.com>
2023-01-05 13:34:13 -0500

    interview-prep-app - version 15 - HomePageInterviewPrep - Features: Migrating all HomePageInterviewPrep content to a topics.page

--- src/pages/home.page.jsx
+++ src/pages/home.page.jsx
@@ -1,52 +1,15 @@
 import React from "react";
-import { Link } from 'react-router-dom'
-import styled from "styled-components";
-import { useAppContext } from "../context";
-
-/**interview-prep-app - version 7 - 
+/**interview-prep-app - version 15 - 
  * HomePageInterviewPrep - Features: 
  * 
- *     --> Setting some white space. 
+ *     --> Migrating all 'HomePageInterviewPrep'
+ *         content to a 'topics.page'. 
  * 
- * Note: to generate url's dinamicly
+ * Note: refactoring also setting a sharedLayout.
  */
 
 const HomePageInterviewPrep = () => {
-
-    const { linksData } = useAppContext()
-    console.log(linksData)
-
-    return(
-        <Wrapper>
-            <div className="topics-container">
-                {linksData.map((item) => {
-                    const { id, url, name } = item;
-
-                    return(
-                        <Link key={id} to={url}>{name}</Link>
-                    )
-                })}
-
-            </div>
-        </Wrapper>
-    )
-}
-
-
-const Wrapper = styled.div`
-    .topics-container{
-        display: flex;
-        justify-content: space-around;
-        margin: 2rem;
-        flex-wrap: wrap;
-
-        a{
-            padding: .5rem;
-            text-transform: capitalize;
-            text-decoration: 2px underline orange;
-            color: deepskyblue;
-        }
-    }
-`
+  return( <h4>Home match with SharedLayout by 'index' attb</h4>);
+};
 
 export default HomePageInterviewPrep;
