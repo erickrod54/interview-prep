743bc239127ad3065d69786aeb3783630b8bbb9f
Erick Rodriguez <erickrod54@gmail.com>
2023-01-06 13:22:14 -0500

    interview-rep-app - version 16 - styled.components.js - Features: Building DataStructuresWrapper

---
+++ src/styled.components.js
@@ -0,0 +1,28 @@
+import styled from "styled-components";
+
+/**interview-prep-app - version 16 - styled.components.js 
+ * - Features: 
+ * 
+ *     --> Building 'DataStructuresWrapper'. 
+ * 
+ * Note: to generate styles for all data structures
+ * topics.
+ */
+
+export const DataStructuresWrapper = styled.div`
+    padding: 1rem;
+    margin: 1rem;
+    display: flex;
+    flex-direction: column;
+    justify-content: center;
+    align-items: center;
+    line-height: 1.5;
+
+    ul{
+        list-style-type: decimal;
+    }
+
+    li{
+        margin: 1rem;
+    }
+`
