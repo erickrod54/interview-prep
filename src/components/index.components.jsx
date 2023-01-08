d9f40278325d3eb6bb8836af47a5fcaa73f142b1
Erick Rodriguez <erickrod54@gmail.com>
2023-01-07 20:25:20 -0500

    interview-prep-app - version 16.01 - components > index - Features: Adding BuildAndUseArrays, Changing versioning to 2 digits

--- src/components/index.components.jsx
+++ src/components/index.components.jsx
@@ -2,17 +2,20 @@ import ArraysImplementations from "./arrays.implementations";
 import BigOnotation from "./big-o-notation.component";
 import BigORuleBook from "./big-o-notation.rules.component";
 import HowToSolveProblems from "./how-to-solve-problems.component";
+import BuildAndUseArrays from "./howtobuild.howtouse.an.array";
 import Nav from "./nav.component";
 import SpaceComplexity from "./space.complexity.component";
 
-/**interview-prep-app - version 13 - 
+/**interview-prep-app - version 16.01 - 
  * components > index - Features: 
  * 
- *     --> Adding 'ArraysImplementations' 
- *          to the index. 
+ *     --> Adding 'BuildAndUseArrays' 
+ *          to the index.
+ * 
+ *     --> Changing versioning to 2 digits. 
  * 
  * Note: the first assets added to 'graphsData'
  * are for big notation app
  */
 
-export { BigOnotation, BigORuleBook, Nav, SpaceComplexity, HowToSolveProblems, ArraysImplementations }
+export { BigOnotation, BigORuleBook, Nav, SpaceComplexity, HowToSolveProblems, ArraysImplementations, BuildAndUseArrays }
