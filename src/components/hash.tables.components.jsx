272c2fabfe7ff943ae682c0b8323d6727768dd13
Erick Rodriguez <erickrod54@gmail.com>
2023-01-12 07:33:21 -0500

    interview-prep-app - version 16.06 - HasTables - Features:  Building HashTables

---
+++ src/components/hash.tables.components.jsx
@@ -0,0 +1,23 @@
+import React from "react";
+import { DataStructuresWrapper } from "../styled.components";
+
+/**interview-prep-app - version 16.06 - HashTables  
+ * - Features: 
+ * 
+ *     --> Building 'HashTables'
+ * 
+ * Note: by id this component make match and render the 
+ * corresponding topic.
+ */
+
+const HashTables = () => {
+
+    return(
+        <DataStructuresWrapper>
+            <h2>Hash Tables Data structure</h2>
+
+        </DataStructuresWrapper>
+    )
+}
+
+export default HashTables;
