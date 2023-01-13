ea7db10c43876c5fd01f9a273cac4d4a06680770
Erick Rodriguez <erickrod54@gmail.com>
2023-01-13 01:39:33 -0500

    interview-prep-app - version 16.08 - components > index - Features: Adding HashCollisions to the index

--- src/components/index.components.jsx
+++ src/components/index.components.jsx
@@ -1,22 +1,21 @@
 import ArraysImplementations from "./arrays.implementations";
 import BigOnotation from "./big-o-notation.component";
 import BigORuleBook from "./big-o-notation.rules.component";
+import HashCollisions from "./hash.collisions";
 import HashTables from "./hash.tables.components";
 import HowToSolveProblems from "./how-to-solve-problems.component";
 import BuildAndUseArrays from "./howtobuild.howtouse.an.array";
 import Nav from "./nav.component";
 import SpaceComplexity from "./space.complexity.component";
 
-/**interview-prep-app - version 16.06 - 
+/**interview-prep-app - version 16.08 - 
  * components > index - Features: 
  * 
- *     --> Adding 'BuildAndUseArrays' 
- *          to the index.
- * 
- *     --> Changing versioning to 2 digits. 
+ *     --> Adding 'HashCollisions' 
+ *          to the index. 
  * 
  * Note: the first assets added to 'graphsData'
  * are for big notation app
  */
 
-export { BigOnotation, BigORuleBook, Nav, SpaceComplexity, HowToSolveProblems, ArraysImplementations, BuildAndUseArrays, HashTables }
+export { BigOnotation, BigORuleBook, Nav, SpaceComplexity, HowToSolveProblems, ArraysImplementations, BuildAndUseArrays, HashTables, HashCollisions }
