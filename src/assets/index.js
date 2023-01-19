9533917e98be47b5aa6953f346bc0d959a54c832
Erick Rodriguez <erickrod54@gmail.com>
2023-01-19 04:03:33 -0500

    interview-prep-app - version 16.09 - assets > index - Features: Importing and exporting hashcollsion asset

--- src/assets/index.js
+++ src/assets/index.js
@@ -2,17 +2,16 @@ import bigo1stgraph from './big-o-graph1.png';
 import bigo2ndgraph from './big-o-graph2.png';
 import bigo3const from './big-o-constant-time-3-op.png';
 import bigoconst from './big-o-constant-time.png';
-import bigochart from './big-o-complexity-chart.png'
+import bigochart from './big-o-complexity-chart.png';
+import hashcollision from './hash-collisions.png'
 
-/**interview-prep-app - version 3 - assets > index - 
+/**interview-prep-app - version 16.09 - assets > index - 
  * Features: 
  * 
- *     --> Importing and exporting assets related
- *          with 'big o complexity constant time' 
- *          to the index.
+ *     --> Importing and exporting 'hashcollision'.
  * 
  * Note: depending on how big will be this component
  * i'll make it's own context
  */
 
-export { bigo1stgraph, bigo2ndgraph, bigoconst, bigo3const, bigochart };
+export { bigo1stgraph, bigo2ndgraph, bigoconst, bigo3const, bigochart, hashcollision };
