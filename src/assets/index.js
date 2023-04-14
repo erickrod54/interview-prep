99b3c607745355384667889238426db63e6ddbc2
Erick Rodriguez <erickrod54@gmail.com>
2023-04-14 01:52:13 -0400

    interview-prep-app - version 17.12 - assets > index - Features: Importing and exporting hashtable

--- src/assets/index.js
+++ src/assets/index.js
@@ -3,15 +3,16 @@ import bigo2ndgraph from './big-o-graph2.png';
 import bigo3const from './big-o-constant-time-3-op.png';
 import bigoconst from './big-o-constant-time.png';
 import bigochart from './big-o-complexity-chart.png';
-import hashcollision from './hash-collisions.png'
+import hashcollision from './hash-collisions.png';
+import hashtable from './data_structure.png';
 
-/**interview-prep-app - version 16.09 - assets > index - 
+/**interview-prep-app - version 17.12 - assets > index - 
  * Features: 
  * 
- *     --> Importing and exporting 'hashcollision'.
+ *     --> Importing and exporting 'hashtable'.
  * 
  * Note: depending on how big will be this component
  * i'll make it's own context
  */
 
-export { bigo1stgraph, bigo2ndgraph, bigoconst, bigo3const, bigochart, hashcollision };
+export { bigo1stgraph, bigo2ndgraph, bigoconst, bigo3const, bigochart, hashcollision, hashtable };
