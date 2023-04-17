3e2e85e7646c395bb9c19a998294bcdeba5f06e8
Erick Rodriguez <erickrod54@gmail.com>
2023-04-17 11:53:47 -0400

    interview-prep-app - version 17.19 - components > index - Features: Adding LinkedLists to the index

--- src/components/index.components.jsx
+++ src/components/index.components.jsx
@@ -7,17 +7,18 @@ import BuildHashTables from "./how-to-build-hash-tables.component";
 import HowToSolveProblems from "./how-to-solve-problems.component";
 import BuildAndUseArrays from "./howtobuild.howtouse.an.array";
 import InterviewQA from "./interview.questons";
+import LinkedLists from "./linked.lists.component";
 import Nav from "./nav.component";
 import SpaceComplexity from "./space.complexity.component";
 
-/**interview-prep-app - version 16.11 - 
+/**interview-prep-app - version 17.19 - 
  * components > index - Features: 
  * 
- *     --> Adding 'InterviewQA' 
+ *     --> Adding 'LinkedLists' 
  *          to the index. 
  * 
  * Note: the first assets added to 'graphsData'
  * are for big notation app
  */
 
-export { BigOnotation, BigORuleBook, Nav, SpaceComplexity, HowToSolveProblems, ArraysImplementations, BuildAndUseArrays, HashTables, HashCollisions, BuildHashTables, InterviewQA }
+export { BigOnotation, BigORuleBook, Nav, SpaceComplexity, HowToSolveProblems, ArraysImplementations, BuildAndUseArrays, HashTables, HashCollisions, BuildHashTables, InterviewQA, LinkedLists }
