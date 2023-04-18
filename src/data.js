96cbad389b5541752edfc002603d460e3d38e7a6
Erick Rodriguez <erickrod54@gmail.com>
2023-04-18 09:07:38 -0400

    interview-prep-app - version 18.00 - data js - Features: Adding linkslistcomposition to graphsData

--- src/data.js
+++ src/data.js
@@ -1,65 +1,65 @@
-import { hashtable, bigo1stgraph, bigo2ndgraph, bigo3const, bigoconst, bigochart, hashcollision, hashtablesvsarrays } from './assets/index';
+import { hashtable, bigo1stgraph, bigo2ndgraph, bigo3const, bigoconst, bigochart, hashcollision, hashtablesvsarrays, linklistcomposition } from './assets/index';
 
-/**interview-prep-app - version 17.19 - data js - 
+/**interview-prep-app - version 18.00 - data js - 
  * Features: 
  * 
- *     --> Adding 'linked lists' to 'linksData'.   
+ *     --> Adding 'linklistcomposition' to 'graphsData'.   
  * 
  * Note: to generate url's dinamicly
  */
 
 export const linksData = [
-    {
-      id:'1',
-      url:'/big-o-notation',
-      name:'big o notation',   
-    },
-    {
-      id:'2',
-      url:'/big-o-rule-book',
-      name:'big o rule-book'
-    },
-    {
-      id:'3',
-      url:'/space-complexity',
-      name:'space complexity'
-    },
-    {
-      id:'4',
-      url:'/how-to-solve-problems',
-      name:'how to solve problems'
-    },
-    {
-      id:'5',
-      url:'/data-structures-and-algorithms',
-      name:'data structures and algorithms'
-    },
-    {
-      id:'6',
-      name:'arrays'
-    },
-    {
-      id:'7',
-      name:'static arrays vs dynamic arrays'
-    },
-    {
-      id:'8',
-      name:'arrays implementation'
-    },
-    {
-      id:'9',
-      name:'Build and use Arrays'
-    },
-    {
-      id:'10',
-      name:'Hash Tables'
-    },
-    {
-      id:'11',
-      name:'hash collisions'
-    },
-    {
-      id:'12',
+  {
+    id:'1',
+    url:'/big-o-notation',
+    name:'big o notation',   
+  },
+  {
+    id:'2',
+    url:'/big-o-rule-book',
+    name:'big o rule-book'
+  },
+  {
+    id:'3',
+    url:'/space-complexity',
+    name:'space complexity'
+  },
+  {
+    id:'4',
+    url:'/how-to-solve-problems',
+    name:'how to solve problems'
+  },
+  {
+    id:'5',
+    url:'/data-structures-and-algorithms',
+    name:'data structures and algorithms'
+  },
+  {
+    id:'6',
+    name:'arrays'
+  },
+  {
+    id:'7',
+    name:'static arrays vs dynamic arrays'
+  },
+  {
+    id:'8',
+    name:'arrays implementation'
+  },
+  {
+    id:'9',
+    name:'Build and use Arrays'
+  },
+  {
+    id:'10',
+    name:'Hash Tables'
+  },
+  {
+    id:'11',
+    name:'hash collisions'
+  },
+  {
+    id:'12',
       name:'how to build hash tables'
     },
     {
@@ -72,68 +72,72 @@ export const linksData = [
     }
     /**
      *,
-    {
-      id:'10',
-      name:'queues'
-    },
-    {
-      id:'11',
-      name:'graphs'
-    },
-    {
-      id:'12',
-      name:'linked lists'
-    },
-    {
-      id:'13',
-      name:'hash tables'
-    },
-     */     
-]
-
-
-
-export const nemo = ['nemo'];
-
-export const graphsData = [
-    {
+     {
+       id:'10',
+       name:'queues'
+      },
+      {
+        id:'11',
+        name:'graphs'
+      },
+      {
+        id:'12',
+        name:'linked lists'
+      },
+      {
+        id:'13',
+        name:'hash tables'
+      },
+      */     
+    ]
+    
+    
+    
+    export const nemo = ['nemo'];
+    
+    export const graphsData = [
+      {
         id:1,
         image:bigo1stgraph
-    },
-    {
+      },
+      {
         id:2,
         image:bigo2ndgraph
-    },
-    {
+      },
+      {
         id:3,
         image:bigoconst
-    },
-    {
+      },
+      {
         id:4,
         image:bigo3const
-    },
-    {
+      },
+      {
         id:5,
         image:bigochart
-    },
-    {
-      id:6,
-      image:hashcollision
-   },
-   {
-      id:7,
-      image: hashtable
-   },
-   {
-      id:8,
-      image: hashtablesvsarrays
-   }
-]
-
-export const everyoneCharacter = [
- 'dory',
- 'bruce',
- 'marlin',
+      },
+      {
+        id:6,
+        image:hashcollision
+      },
+      {
+        id:7,
+        image: hashtable
+      },
+      {
+        id:8,
+        image: hashtablesvsarrays
+      },
+      {
+        id:9,
+        image:linklistcomposition
+      }
+    ]
+    
+    export const everyoneCharacter = [
+      'dory',
+      'bruce',
+      'marlin',
  'nemo',
  'gill',
  'bloat',
