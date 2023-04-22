b535e381dc849a74dd2874a6cf6fab7d8074c2dd
Erick Rodriguez <erickrod54@gmail.com>
2023-04-21 22:20:07 -0400

    interview-prep-app - version 18.13 - components > index - Features: Adding DoublyLinkedList to the index

--- src/components/index.components.jsx
+++ src/components/index.components.jsx
@@ -1,6 +1,7 @@
 import ArraysImplementations from "./arrays.implementations";
 import BigOnotation from "./big-o-notation.component";
 import BigORuleBook from "./big-o-notation.rules.component";
+import DoublyLinkedList from "./doubly.linked.lists.component";
 import HashCollisions from "./hash.collisions";
 import HashTables from "./hash.tables.components";
 import BuildHashTables from "./how-to-build-hash-tables.component";
@@ -11,14 +12,14 @@ import LinkedLists from "./linked.lists.component";
 import Nav from "./nav.component";
 import SpaceComplexity from "./space.complexity.component";
 
-/**interview-prep-app - version 17.19 - 
+/**interview-prep-app - version 18.13 - 
  * components > index - Features: 
  * 
- *     --> Adding 'LinkedLists' 
+ *     --> Adding 'DoublyLinkedList' 
  *          to the index. 
  * 
  * Note: the first assets added to 'graphsData'
  * are for big notation app
  */
 
-export { BigOnotation, BigORuleBook, Nav, SpaceComplexity, HowToSolveProblems, ArraysImplementations, BuildAndUseArrays, HashTables, HashCollisions, BuildHashTables, InterviewQA, LinkedLists }
+export { BigOnotation, DoublyLinkedList, BigORuleBook, Nav, SpaceComplexity, HowToSolveProblems, ArraysImplementations, BuildAndUseArrays, HashTables, HashCollisions, BuildHashTables, InterviewQA, LinkedLists }
