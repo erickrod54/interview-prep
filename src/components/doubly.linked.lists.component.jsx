2d23ed8f789f22c6110b07574f09a998db3924f7
Erick Rodriguez <erickrod54@gmail.com>
2023-04-22 02:07:26 -0400

    interview-prep-app - version 18.13 - DoublyLinkedList - Features: Building doubly linked lists basic Component

---
+++ src/components/doubly.linked.lists.component.jsx
@@ -0,0 +1,23 @@
+import React from "react";
+import { DataStructuresWrapper } from "../styled.components";
+
+/**interview-prep-app - version 18.13 - DoublyLinkedList - 
+ * Features: 
+ * 
+ *     --> Building 'doubly linked lists' basic Component.   
+ * 
+ * Note: DoublyLinkedLIst is also a linked list and in this
+ * component is going to be develop the concept
+ */
+
+const DoublyLinkedList = () => {
+    
+    return(
+        <DataStructuresWrapper>
+            <h2>Doubly LinkedList</h2>
+
+        </DataStructuresWrapper>
+    )
+}
+
+export default DoublyLinkedList;
