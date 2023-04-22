dd1540f94efc637b96644ba3dabdf8ba161ebb14
Erick Rodriguez <erickrod54@gmail.com>
2023-04-22 02:38:58 -0400

    interview-prep-app - version 18.15 - pages > index  - Features: Testing the components imports SingleTopicPage, TopicsPage

--- src/pages/index.pages.jsx
+++ src/pages/index.pages.jsx
@@ -1,4 +1,15 @@
 import HomePageInterviewPrep from "./home.page";
+import SingleTopicPage from "./single.topic.page";
+import TopicsPage from "./topics.page";
 
+/**interview-prep-app - version 18.15 - 
+ * pages > index - Features: 
+ * 
+ *     --> Testing the components imports
+ *          SingleTopicPage, TopicsPage.
+ * 
+ * Note: the first assets added to 'graphsData'
+ * are for big notation app
+ */
 
-export { HomePageInterviewPrep }
+export { HomePageInterviewPrep, SingleTopicPage, TopicsPage }
