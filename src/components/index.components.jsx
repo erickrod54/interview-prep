c90c048492143931fd95c9a9269e3652504fc6b6
Erick Rodriguez <erickrod54@gmail.com>
2023-04-22 02:28:13 -0400

    interview-prep-app - version 18.14 - components > index - Features: Testing the components imports

--- src/components/index.components.jsx
+++ src/components/index.components.jsx
@@ -3,23 +3,44 @@ import BigOnotation from "./big-o-notation.component";
 import BigORuleBook from "./big-o-notation.rules.component";
 import DoublyLinkedList from "./doubly.linked.lists.component";
 import HashCollisions from "./hash.collisions";
+import ArrayDataStructure from './array-data-structure';
+import DynamicVsStatic from './arrays-dynamic-vs-static';
+import DataStructures from './data-structures-and-algorithms';
+import Nav from "./nav.component";
 import HashTables from "./hash.tables.components";
 import BuildHashTables from "./how-to-build-hash-tables.component";
 import HowToSolveProblems from "./how-to-solve-problems.component";
 import BuildAndUseArrays from "./howtobuild.howtouse.an.array";
 import InterviewQA from "./interview.questons";
 import LinkedLists from "./linked.lists.component";
-import Nav from "./nav.component";
 import SpaceComplexity from "./space.complexity.component";
 
-/**interview-prep-app - version 18.13 - 
+
+/**interview-prep-app - version 18.14 - 
  * components > index - Features: 
  * 
- *     --> Adding 'DoublyLinkedList' 
- *          to the index. 
+ *     --> Testing the components imports
  * 
  * Note: the first assets added to 'graphsData'
  * are for big notation app
  */
 
-export { BigOnotation, DoublyLinkedList, BigORuleBook, Nav, SpaceComplexity, HowToSolveProblems, ArraysImplementations, BuildAndUseArrays, HashTables, HashCollisions, BuildHashTables, InterviewQA, LinkedLists }
+//export { BigOnotation, DoublyLinkedList, BigORuleBook, Nav, SpaceComplexity, HowToSolveProblems, ArraysImplementations, BuildAndUseArrays, HashTables, HashCollisions, BuildHashTables, InterviewQA, LinkedLists }
+
+export { ArrayDataStructure,
+         DynamicVsStatic,
+         ArraysImplementations,
+         BigOnotation,
+         BigORuleBook,
+         DataStructures,
+         DoublyLinkedList,
+         HashCollisions,
+         Nav,
+         HashTables,
+         BuildHashTables,
+         HowToSolveProblems,
+         BuildAndUseArrays,
+         InterviewQA,
+         LinkedLists,
+         SpaceComplexity
+        }
