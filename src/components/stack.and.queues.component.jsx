8130713990a1c1f7e46c50e81a7c059e5ca113d4
Erick Rodriguez <erickrod54@gmail.com>
2023-05-03 12:43:58 -0400

    interview-prep-app - version 20.05 - StackAndQueues - Features: Building StackAndQueues inital component

---
+++ src/components/stack.and.queues.component.jsx
@@ -0,0 +1,24 @@
+import React from "react";
+import { DataStructuresWrapper } from "../styled.components";
+
+/**interview-prep-app - version 20.05 - StackAndQueues - 
+ * Features: 
+ * 
+ *     --> Building 'StackAndQueues' initial
+ *         component.   
+ * 
+ * Note: to generate url's dinamicly
+ */
+
+const StackAndQueues = () => {
+    
+    return(
+        
+        <DataStructuresWrapper>
+            <h2>Stack and Queues</h2>
+            
+        </DataStructuresWrapper>
+    )
+}
+
+export default StackAndQueues
