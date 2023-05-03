ec0b6c75a5074bb4e9656114f440d07d6a653745
Erick Rodriguez <erickrod54@gmail.com>
2023-05-03 13:23:10 -0400

    interview-prep-app - version 20.06 - StackAndQueues - Features: Work in progress developing StackAndQueues

--- src/components/stack.and.queues.component.jsx
+++ src/components/stack.and.queues.component.jsx
@@ -1,11 +1,10 @@
 import React from "react";
 import { DataStructuresWrapper } from "../styled.components";
 
-/**interview-prep-app - version 20.05 - StackAndQueues - 
+/**interview-prep-app - version 20.06 - StackAndQueues - 
  * Features: 
  * 
- *     --> Building 'StackAndQueues' initial
- *         component.   
+ *     --> Work in progress developing 'Stack and Queues'.   
  * 
  * Note: to generate url's dinamicly
  */
@@ -16,6 +15,25 @@ const StackAndQueues = () => {
         
         <DataStructuresWrapper>
             <h2>Stack and Queues</h2>
+
+            <p>
+                <strong> 'Stack and Queues' </strong> are similar and they are 
+                <strong> linear strcutures </strong> that allows to traverse 
+                sequentially throught the data, the difference is how items 
+                get removed from on to another, there is no random access 
+                operations, and they can only access from the <strong> first or the last
+                data </strong> item
+            </p>
+
+            <p>
+                the limitation to access only throught the <strong> first or the last
+                data </strong>, unlike hash tables or linked list i have less 
+                methods with <strong> 'Stack and Queues' </strong>,  and this
+                represents a benefit, because i can build hash tables and 
+                linked list over <strong> 'Stack and Queues' </strong> to 
+                limit the permforming operations resulting in a better
+                control over the data
+            </p>
             
         </DataStructuresWrapper>
     )
