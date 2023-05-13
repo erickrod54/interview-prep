75a3e1a16931b5672ddb9310057aa52bd5f4b218
Erick Rodriguez <erickrod54@gmail.com>
2023-05-12 21:30:00 -0400

    interview-prep-app - version 21.12 - StackAndLinkedList - Features: Work in progress developing a linked list for the Stack

--- src/components/stack.linked.list.component.jsx
+++ src/components/stack.linked.list.component.jsx
@@ -1,10 +1,11 @@
 import React from "react";
 import { DataStructuresWrapper } from "../styled.components";
 
-/**interview-prep-app - version 21.11 - StacksAndLinkedList  
+/**interview-prep-app - version 21.12 - StacksAndLinkedList  
  * - Features: 
  * 
- *     --> Building basic 'StacksAndLinkedList'. 
+ *     --> Work in progress developing a Linked 
+ *         List for the Stack. 
  * 
  * Note: In this component is going to be develop 
  * how stacks works with linked list to create a new
@@ -12,6 +13,37 @@ import { DataStructuresWrapper } from "../styled.components";
  */
 
 const StacksAndLinkedList = () => {
+
+    class Node {
+        constructor(value) {
+            this.value = value;
+            this.next = null;
+        }
+    }
+
+    class Stack {
+        constructor(){
+            this.top = null;
+            this.bottom = null;
+            this.length = 0
+        }
+
+        peek(){
+
+        }
+
+        push(value){
+
+        }
+
+        pop(){
+
+        }
+
+        //
+    }
+
+    const myStack = new Stack();
 
     return(
         <DataStructuresWrapper>
