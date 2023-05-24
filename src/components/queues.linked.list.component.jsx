f8812b9ad867660ff913fe579cb1f1f872cc4ab6
Erick Rodriguez <erickrod54@gmail.com>
2023-05-23 21:30:00 -0400

    interview-prep-app - version 24.01 - QueuesAndlinkedList - Features: Building QueuesAndLinkedList basic component

---
+++ src/components/queues.linked.list.component.jsx
@@ -0,0 +1,21 @@
+import React from "react";
+import { DataStructuresWrapper } from "../styled.components";
+
+/**interview-prep-app - version 24.01 - QueueAndLinkedList  
+ * - Features: 
+ * 
+ *     --> Building 'QueueAndLinkedList' basic component.
+ * 
+ * Note: changes are going to be made to this component
+ */
+
+const QueueAndLinkedList = () => {
+    
+    return(
+        <DataStructuresWrapper>
+            <h2>QueueAndLinkedList</h2>
+        </DataStructuresWrapper>
+    )
+}
+
+export default QueueAndLinkedList
