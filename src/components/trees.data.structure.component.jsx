3bfd67f61c7ffa258ca1e5d2892c875f38f53304
Erick Rodriguez <erickrod54@gmail.com>
2023-05-27 19:30:00 -0400

    interview-prep-app - version 24.20 - TreesDataStructure - Features: Building TreesDataStructure basic Component

---
+++ src/components/trees.data.structure.component.jsx
@@ -0,0 +1,22 @@
+import React from "react";
+import { DataStructuresWrapper } from "../styled.components";
+
+/**interview-prep-app - version 24.20 - 
+ * TreeDataStructure - Features: 
+ * 
+ *     --> Building 'TreeDataStructure' Basic Component.
+ * 
+ * Note: the first assets added to 'graphsData'
+ * are for big notation app
+ */
+
+const TreeDataStructure = () => {
+
+    return(
+        <DataStructuresWrapper>
+            <h2>Tree Data Structure</h2>
+        </DataStructuresWrapper>
+    )
+}
+
+export default TreeDataStructure;
