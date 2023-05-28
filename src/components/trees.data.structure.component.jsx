cb99e91c1ca261798b6d55e9353749f1314f047b
Erick Rodriguez <erickrod54@gmail.com>
2023-05-28 19:30:00 -0400

    interview-prep-app - version 25.00 - TreeDataStructure - Features: Destructuring graphsData from the context

--- src/components/trees.data.structure.component.jsx
+++ src/components/trees.data.structure.component.jsx
@@ -1,16 +1,21 @@
 import React from "react";
+import { useAppContext } from "../context";
 import { DataStructuresWrapper } from "../styled.components";
 
-/**interview-prep-app - version 24.20 - 
+/**interview-prep-app - version 25.00 - 
  * TreeDataStructure - Features: 
  * 
- *     --> Building 'TreeDataStructure' Basic Component.
+ *     --> Destructuring graphsData from the context.
  * 
  * Note: the first assets added to 'graphsData'
  * are for big notation app
  */
 
 const TreeDataStructure = () => {
+
+    const { graphsData } = useAppContext();
+
+    console.log(' graphsData ==>', graphsData)
 
     return(
         <DataStructuresWrapper>
