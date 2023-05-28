f1012b5ecd22926dcef67705dd3f7762a7fc112b
Erick Rodriguez <erickrod54@gmail.com>
2023-05-28 19:30:00 -0400

    interview-prep-app - version 25.02 - TreeDataStructure - Features: Developing Tree Data Structure concept, Placing treesdatastructure asset image

--- src/components/trees.data.structure.component.jsx
+++ src/components/trees.data.structure.component.jsx
@@ -2,12 +2,12 @@ import React from "react";
 import { useAppContext } from "../context";
 import { DataStructuresWrapper } from "../styled.components";
 
-/**interview-prep-app - version 25.01 - 
+/**interview-prep-app - version 25.02 - 
  * TreeDataStructure - Features: 
  * 
- *     --> Pulling 'treesdatastructure' from 'graphsData'.
+ *     --> Developing Tree Data Structure concept.
  * 
- *     --> Developing Tree Data Structure concept
+ *     --> Placing 'treesdatastructure' asset image
  * 
  * Note: the first assets added to 'graphsData'
  * are for big notation app
@@ -32,6 +32,8 @@ const TreeDataStructure = () => {
                 inverse tree, where the <strong> top of the tree is the parent and the leaves descend from there </strong>, 
                 and there are leaves that in turn are parents of other leaves, as <strong> is the case of '1' and '3' </strong>
             </p>
+
+            <img src={treesdatastructure} alt='trees data structure'/>
 
             <p>
                 Linked List is a kind of tree because each parent has a child related in <strong> 'unidirectional way' </strong>
