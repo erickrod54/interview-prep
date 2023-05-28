abe4850d163fa102cc711fef4b7721075498a061
Erick Rodriguez <erickrod54@gmail.com>
2023-05-28 19:30:00 -0400

    interview-prep-app - version 25.01 - TreeDataStructure - Features: Pulling treedatastructure from graphsData, Developing Tree Data Structure concept

--- src/components/trees.data.structure.component.jsx
+++ src/components/trees.data.structure.component.jsx
@@ -2,10 +2,12 @@ import React from "react";
 import { useAppContext } from "../context";
 import { DataStructuresWrapper } from "../styled.components";
 
-/**interview-prep-app - version 25.00 - 
+/**interview-prep-app - version 25.01 - 
  * TreeDataStructure - Features: 
  * 
- *     --> Destructuring graphsData from the context.
+ *     --> Pulling 'treesdatastructure' from 'graphsData'.
+ * 
+ *     --> Developing Tree Data Structure concept
  * 
  * Note: the first assets added to 'graphsData'
  * are for big notation app
@@ -17,9 +19,24 @@ const TreeDataStructure = () => {
 
     console.log(' graphsData ==>', graphsData)
 
+    const treesdatastructure = graphsData[16].image;
+
     return(
         <DataStructuresWrapper>
+
             <h2>Tree Data Structure</h2>
+
+            <p>
+                The tree is a data structure in which the parent is related to a 
+                son in a <strong> 'unidirectional way' </strong>, as can be seen in the image, it is an 
+                inverse tree, where the <strong> top of the tree is the parent and the leaves descend from there </strong>, 
+                and there are leaves that in turn are parents of other leaves, as <strong> is the case of '1' and '3' </strong>
+            </p>
+
+            <p>
+                Linked List is a kind of tree because each parent has a child related in <strong> 'unidirectional way' </strong>
+                and it is <strong> hierarchical </strong>
+            </p>
         </DataStructuresWrapper>
     )
 }
