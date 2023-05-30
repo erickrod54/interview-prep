99193b01f8117c1b4fe2d32423a199844a052310
Erick Rodriguez <erickrod54@gmail.com>
2023-05-29 22:44:46 -0400

    interview-prep-app - version 25.03 - TreeDataStructure - Features: Developing Tree Data Structure Concept, Developing Binary Tree Concept

--- src/components/trees.data.structure.component.jsx
+++ src/components/trees.data.structure.component.jsx
@@ -2,15 +2,14 @@ import React from "react";
 import { useAppContext } from "../context";
 import { DataStructuresWrapper } from "../styled.components";
 
-/**interview-prep-app - version 25.02 - 
+/**interview-prep-app - version 25.03 - 
  * TreeDataStructure - Features: 
  * 
  *     --> Developing Tree Data Structure concept.
  * 
- *     --> Placing 'treesdatastructure' asset image
+ *     --> Developing 'Binary Tree' Concept
  * 
- * Note: the first assets added to 'graphsData'
- * are for big notation app
+ * Note: pending to add binary tree assets
  */
 
 const TreeDataStructure = () => {
@@ -37,8 +36,37 @@ const TreeDataStructure = () => {
 
             <p>
                 Linked List is a kind of tree because each parent has a child related in <strong> 'unidirectional way' </strong>
-                and it is <strong> hierarchical </strong>
+                and it is <strong> hierarchical </strong> so as in the representation in the image this tree is compose
+                in the following way:
             </p>
+
+            <ul>
+                <li>Root <strong> '1' </strong></li>
+                <li>Parent <strong> '1' and '3'</strong></li>
+                <li>Child <strong> '2', '3', '4', '6', '7'</strong></li>
+                <li>Leaf <strong> '2', '4', '6', '7'</strong></li>
+                <li>Siblings <strong> '2', '3', '4', '6', '7' </strong></li>
+            </ul>
+
+            <p>
+                Lets talk about the most use trees data structures, there is many kinds of tree,
+                but i will focus in the most use ( check the reference to access )
+            </p>
+
+            <h3>Binary Tree</h3>
+
+            <p>
+                A binary tree is a tree that has <strong> 'two node child' </strong>, and <strong> their child have exactly two node child </strong> 
+            </p>
+
+            <p>
+                from this binary tree i will focus in two classifications:
+            </p>
+
+            <ul>
+                <li>Perfect Binary tree</li>
+                <li>Full Binary tree</li>
+            </ul>
         </DataStructuresWrapper>
     )
 }
