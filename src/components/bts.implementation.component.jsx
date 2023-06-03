82f21b45145bd32cf63ec52e68d58da42b20c3e5
Erick Rodriguez <erickrod54@gmail.com>
2023-06-03 10:34:37 -0400

    interview-prep-app - version 25.18 - BTSComponent - Features: Building Basic BTSComponent

---
+++ src/components/bts.implementation.component.jsx
@@ -0,0 +1,23 @@
+import React from "react";
+import { DataStructuresWrapper } from "../styled.components";
+
+/**interview-prep-app - version 25.18 - 
+ * BTSComponent - Features: 
+ * 
+ *     --> Building Basic 'BTSComponent'.
+ * 
+ * Note: the first assets added to 'graphsData'
+ * are for big notation app
+ */
+
+const BTSComponent = () => {
+
+    return(
+        <DataStructuresWrapper>
+            <h2>BTS Implementation ( Binary Search Tree - BTS )</h2>
+
+        </DataStructuresWrapper>
+    )
+}
+
+export default BTSComponent;
