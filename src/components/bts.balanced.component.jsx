4da638f8249bacad903f0c25bbfb33b796935e61
Erick Rodriguez <erickrod54@gmail.com>
2023-06-09 02:00:55 -0400

    interview-prep-app - version 27.07 - SingleTopicPage - Features: Developing BTS Balanced concept

--- src/components/bts.balanced.component.jsx
+++ src/components/bts.balanced.component.jsx
@@ -1,10 +1,10 @@
 import React from "react";
 import { DataStructuresWrapper } from "../styled.components";
 
-/**interview-prep-app - version 27.06 - BTSBalanced  
+/**interview-prep-app - version 27.07 - BTSBalanced  
  * - Features: 
  * 
- *     --> Building Basic BTSBalanced component. 
+ *     --> Developing BTS Balanced concept. 
  * 
  * Note: Pending to re structure imports using the index.
  */
@@ -14,6 +14,20 @@ const BTSBalanced = () => {
     return(
         <DataStructuresWrapper>
             <h2>BTS Balanced</h2>
+
+            <p>
+                there are two ways of make a tree <strong> 'balance' </strong>, and 
+                it is achieved by using:
+            </p>
+
+            <ul>
+                <li>
+                    <strong> AVL tree </strong>
+                </li>
+                <li>
+                    <strong> Red Black tree </strong>
+                </li>
+            </ul>
 
         </DataStructuresWrapper>
     )
