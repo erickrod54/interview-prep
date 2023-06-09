cb1ff54f8d536f5a0a28da7846441a45c5cd4600
Erick Rodriguez <erickrod54@gmail.com>
2023-06-09 01:53:21 -0400

    interview-prep-app - version 27.06 - BTSBalanced - Features: Building Basic BTSBalanced component

---
+++ src/components/bts.balanced.component.jsx
@@ -0,0 +1,22 @@
+import React from "react";
+import { DataStructuresWrapper } from "../styled.components";
+
+/**interview-prep-app - version 27.06 - BTSBalanced  
+ * - Features: 
+ * 
+ *     --> Building Basic BTSBalanced component. 
+ * 
+ * Note: Pending to re structure imports using the index.
+ */
+
+const BTSBalanced = () => {
+
+    return(
+        <DataStructuresWrapper>
+            <h2>BTS Balanced</h2>
+
+        </DataStructuresWrapper>
+    )
+}
+
+export default BTSBalanced;
