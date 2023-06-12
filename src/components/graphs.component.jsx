ad3fb3920e1b4561d87cda4d43a58c14672983a1
Erick Rodriguez <erickrod54@gmail.com>
2023-06-12 14:30:48 -0400

    interview-prepa-app - version 27.13 - GraphsStructure - Features: Developing Graphs Data Structure concept, Destructuring graphs from the context

--- src/components/graphs.component.jsx
+++ src/components/graphs.component.jsx
@@ -1,16 +1,24 @@
 import React from "react";
 import { DataStructuresWrapper } from "../styled.components";
+import { useAppContext } from "../context";
 
-/**interview-prep-app - version 27.12 - 
+/**interview-prep-app - version 27.13 - 
  * GraphsStructure - Features: 
  * 
  *     --> Developing 'Graphs Data Structure' concept.
+ * 
+ *     --> Destructuring 'graphs' from the context.
  * 
  * Note: the first assets added to 'graphsData'
  * are for big notation app
  */
 
 const GraphsStructure = () => {
+
+    const { graphsData } = useAppContext()
+    console.log(' graphsData ==> ', graphsData)
+
+    const graphs = graphsData[24].image;
 
     return(
         <DataStructuresWrapper>
@@ -24,7 +32,10 @@ const GraphsStructure = () => {
                 how internet works, or maybe friendships, family trees, networks in the world wide web,
                 roads ( for example amazon use it for their recommendation engines, google maps 
                 to calculate the shortest path where you want to go the roads, facebook for their social network )
+                as the following image i can appreciate how it is compose:
             </p>
+
+            <img src={graphs} className='large' alt="graphs"/>
             
         </DataStructuresWrapper>
     )
