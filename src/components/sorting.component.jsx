3889b7b3210319137638e53a391f3292f13c354e
Erick Rodriguez <erickrod54@gmail.com>
2023-07-15 21:30:00 -0400

    interview-prep-app - version 31.04 - SortingComponent - Features: Building quicksort handler

--- src/components/sorting.component.jsx
+++ src/components/sorting.component.jsx
@@ -2,10 +2,10 @@ import React, { useState } from "react";
 import { DataStructuresWrapper } from "../styled.components";
 import { useAppContext } from "../context";
 
-/**interview-prep-app - version 31.03 - SortingComponent  
+/**interview-prep-app - version 31.04 - SortingComponent  
  * - Features: 
  * 
- *     --> Building 'quicksortvalue' state.
+ *     --> Building 'quickSort' handler.
  * 
  * Note: Quick sort is going to be applied over
  * numebrs array
@@ -51,6 +51,46 @@ const SortingComponent = () => {
     const spanish = ['único', 'árbol', 'cosas', 'fútbol'];
     
     const numbers = [99, 44, 6, 2, 1, 5, 63, 87,283, 4, 0 ];
+
+    const quickSort = (array, left, right) => {
+        const len = array.length; 
+        let pivot;
+        let partitionIndex;
+      
+        if(left < right) {
+          pivot = right;
+          partitionIndex = partition(array, pivot, left, right);
+          
+          //sort left and right
+
+          /**the pivot is the last item 'partitionIndex - 1' of each 
+           * split list ( this process is ramdom )*/
+          quickSort(array, left, partitionIndex - 1);
+          quickSort(array, partitionIndex + 1, right);
+        }
+        setQuicksortvalue(array + ',' );
+        return array;
+      }
+         
+      function partition(array, pivot, left, right){
+        let pivotValue = array[pivot];
+        let partitionIndex = left;
+      
+        for(let i = left; i < right; i++) {
+          if(array[i] < pivotValue){
+            swap(array, i, partitionIndex);
+            partitionIndex++;
+          }
+        }
+        swap(array, right, partitionIndex);
+        return partitionIndex;
+      }
+      
+      function swap(array, firstIndex, secondIndex){
+          var temp = array[firstIndex];
+          array[firstIndex] = array[secondIndex];
+          array[secondIndex] = temp;
+      }
     
     const length = numbers.length;
     const middle = Math.floor(length / 2 );
