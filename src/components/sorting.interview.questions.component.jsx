982d3a6894169177502f519201b6b7e260d23bd8
Erick Rodriguez <erickrod54@gmail.com>
2023-07-17 08:47:50 -0400

    interview-prep-app - version 31.11 - SortingInterview - features: Building basic SortingInterview Component

---
+++ src/components/sorting.interview.questions.component.jsx
@@ -0,0 +1,21 @@
+import React from "react";
+import { DataStructuresWrapper } from "../styled.components";
+
+/**interview-prep-app - version 31.11 - SortingInterview   
+ * - Features: 
+ * 
+ *     --> Building basic 'SortingInterview ' Component. 
+ * 
+ * Note: Pending to re structure imports using the index.
+ */
+
+const SortingInterview = () => {
+
+    return(
+        <DataStructuresWrapper>
+            <h2>Sorting Interview Questions:</h2>
+        </DataStructuresWrapper>
+    )
+}
+
+export default SortingInterview;
