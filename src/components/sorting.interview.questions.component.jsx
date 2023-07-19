190f24f623841f10eac682a7e06b3b1de1ff0a0c
Erick Rodriguez <erickrod54@gmail.com>
2023-07-18 21:30:00 -0400

    interview-prep-app - version 31.15 - SortingInterview - Features: Completing answers to the interview questions

--- src/components/sorting.interview.questions.component.jsx
+++ src/components/sorting.interview.questions.component.jsx
@@ -2,11 +2,10 @@ import React from "react";
 import { DataStructuresWrapper } from "../styled.components";
 import { useAppContext } from "../context";
 
-/**interview-prep-app - version 31.14 - SortingInterview   
+/**interview-prep-app - version 31.15 - SortingInterview   
  * - Features: 
  * 
- *     --> Destructuring 'sortingalgorithmsperformancetable'
- *         from the context. 
+ *     --> Completing answers to the interview questions. 
  * 
  * Note: Pending to re structure imports using the index.
  * pending to add an extra resource 'sortingalgorithmsperformancetable' 
@@ -32,49 +31,68 @@ const SortingInterview = () => {
                 <li><strong> Sort 10 schools around your house by distance </strong></li>
 
                 <p>
-                    R:
+                    R: First of all it state about a small ammount so match of the description of 
+                       small ammount of data ( could be partially sorted ), so i can use 
+                       <strong> Insertion Sort </strong> 
                 </p>
 
                 <li><strong> eBay sorts listings by the current Bid amount </strong></li>
 
                 <p>
-                    R:
+                    R: In this case it state about <strong> 'eBay Bid' </strong> that should be 
+                       a big list of integers data, so ideal sorting algoritmhs to treat numbers 
+                       are <strong> Radix or Counting Sort </strong> ( it is a fixed lenght of numbers )
                 </p>
 
                 <li><strong> Sort scores on ESPN</strong></li>
 
                 <p>
-                    R:
+                    R: By nature of this data, socores can be <strong> decimal </strong> and vary of their format
+                       so for this case <strong> Quick Sort </strong> it is a good approach for this use case 
+                       because can handle efficiently in <strong> space complexity </strong>, because of their 
+                       diverse formats <strong> Quick Sort </strong> can handle it as one same set ( avoiding )
+                       unnecessary memory increasing
                 </p>
 
                 <li><strong> Massive database (can't fit all into memory) needs to sort through past year's user data</strong></li>
 
                 <p>
-                    R:
+                    R: Base on the information, is a big ammount of data that need to be sorted in some type 
+                       of <strong> data </strong>, so for this use case a good approach will be <strong> Merge Sort </strong>
+                       because as the same of <strong> Quick Sort </strong> can handle worst case scenarios of 
+                       <strong> complexity ( in time and space ) </strong> very efficiently ( and it is stating a really large 
+                       data ammount, so i have to focus on performance, because it is already a worst case scenerario in 
+                       space complexity, so what i need is <strong> fast sorting </strong> )
                 </p>
 
                 <li><strong> Almost sorted Udemy review data needs to update and add 2 new reviews</strong></li>
 
                 <p>
-                    R:
+                    R: It state about a big ammount of data, and it is  <strong> almost sorted </strong> so i can 
+                       take this use case with a <strong> insertion sort </strong> approach, which can result in 
+                       a good performance due to the ammount of data almost sorted
                 </p>
 
                 <li><strong> Temperature Records for the past 50 years in Canada</strong></li>
 
                 <p>
-                    R:
+                    R: The temperature has decimal places so a good approach will be <strong> Quick Sort </strong>,
+                       it will sort efficiently perhaphs will have some <strong> space complexity </strong> increase 
+                       in order to sort the listing but it will handle efficiently the treatment of <strong> decimal data </strong>
                 </p>
 
                 <li><strong> Large user name database needs to be sorted. Data is very random</strong></li>
 
                 <p>
-                    R:
+                    R: It is a very Random data so i cannot use algorithms that benefits of almost sorted data sets 
+                       so will take this use case with a <strong> Merge Sort approach </strong>, because i do not 
+                       much information about the data's nature, but it states that is random
                 </p>
 
                 <li><strong> You want to teach sorting for the first time</strong></li>
 
                 <p>
-                    R:
+                    R: <strong> Bubble sort or Selection sort  </strong> definitively
                 </p>
 
                 <h2>Which Sort is the Best ?</h2>
@@ -131,6 +149,11 @@ const SortingInterview = () => {
             </ul>
 
             <img src={sortingalgorithmsperformancetable} className="large" alt="sorting algorithms performance table"/> 
+
+            <h4> Note: depending on the browser and the motor engine that uses (for example chrome uses V8 ), the '.sort()' ( the default method ),
+                 uses a sorting algorithm ( <a href="https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Array/sort"> check the reference to the .sort() implementation</a> ) of the browser
+            </h4>
+            
         </DataStructuresWrapper>
     )
 }
