edcb64def9a64fdaa3a77f4308dcd18e45d7a829
Erick Rodriguez <erickrod54@gmail.com>
2023-07-18 21:30:00 -0400

    interview-prep-app - version 31.16 - SearchingAlgorithms - Features: Building basic SearchingAlgorithms Component

---
+++ src/components/searching.algorithms.component.jsx
@@ -0,0 +1,24 @@
+import React from "react";
+import { DataStructuresWrapper } from "../styled.components";
+
+
+/**interview-prep-app - version 31.16 - 
+ * SearchingAlgorithms - Features: 
+ * 
+ *     --> Building basic 'SearchingAlgorithms' Component.
+ * 
+ * Note: the first assets added to 'graphsData'
+ * are for big notation app
+ */
+
+const SearchingAlgorithms = () => {
+
+    return(
+        <DataStructuresWrapper>
+            <h2> Searching Algorithms</h2>
+
+        </DataStructuresWrapper>
+    )
+}
+
+export default SearchingAlgorithms;
