69e8e2412bb1ac15b689291fa49434d7baaf475d
Erick Rodriguez <erickrod54@gmail.com>
2023-07-19 21:30:00 -0400

    interview-prep-app - version 31.17 - SearchingAlgorithms - Features: Developing Searching Algorithmhs concept

--- src/components/searching.algorithms.component.jsx
+++ src/components/searching.algorithms.component.jsx
@@ -2,10 +2,10 @@ import React from "react";
 import { DataStructuresWrapper } from "../styled.components";
 
 
-/**interview-prep-app - version 31.16 - 
+/**interview-prep-app - version 31.17 - 
  * SearchingAlgorithms - Features: 
  * 
- *     --> Building basic 'SearchingAlgorithms' Component.
+ *     --> Developing 'Searching Algorithms' concept.
  * 
  * Note: the first assets added to 'graphsData'
  * are for big notation app
@@ -15,7 +15,14 @@ const SearchingAlgorithms = () => {
 
     return(
         <DataStructuresWrapper>
+
             <h2> Searching Algorithms</h2>
+
+            <p>
+                <strong> Searching and Traversal </strong> is incredible useful, and it is a big part of life 
+                because their every day implementation ( searching files in the computer, web search and even 
+                more wide searching in big data ), so it is an important topic
+            </p>
 
         </DataStructuresWrapper>
     )
