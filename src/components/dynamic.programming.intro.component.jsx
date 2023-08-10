1e9d0a4928866dfe5bff5f350fde4ff9f37ba284
Erick Rodriguez <erickrod54@gmail.com>
2023-08-10 12:23:14 -0400

    interview-prep-app - version 34.18 - DynamicProgramming - Features: Developing DynamicProgramming Concept

--- src/components/dynamic.programming.intro.component.jsx
+++ src/components/dynamic.programming.intro.component.jsx
@@ -1,9 +1,9 @@
 import React from "react";
 import { DataStructuresWrapper } from "../styled.components";
 
-/**interview-prep-app - version 34.14-  DynamicProgramming - Features: 
+/**interview-prep-app - version 34.18-  DynamicProgramming - Features: 
  * 
- *     --> Building basic 'DynamicProgramming' Component.
+ *     --> Developing 'DynamicProgramming' Concept.
  * 
  * Note: this 'handlerDFSPostorder' will handle the array filling the queue
  * with the post order.
@@ -14,7 +14,17 @@ const DynamicProgramming = () => {
     return(
         <DataStructuresWrapper>
             <h2>Dynamic Programming</h2>
-            
+            <p>
+                Dynamic Programming is an <strong> 'optimization technique' </strong> ( do you have somethinf you can cache ? ),
+                so what it does this technique is <strong> 'store' repetitive high cost operations </strong>, ( this process 
+                has a tradeoff, because bhy storing space complexity grow ) with the main goal of improve significally time 
+                complexity, by applying this technique time complexity of <strong> () </strong>
+            </p>
+
+            <p>
+                DynamicProgramming can adopt names as <strong> memoization - caching </strong>, by applying this technique 
+                <strong> speeds up programs </strong> ( better time complexity ) make it them more accesible
+            </p>
         </DataStructuresWrapper>
     )
 }
