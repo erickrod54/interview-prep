3a89f893335710733f9d4af9e2e96e4b9893ce76
Erick Rodriguez <erickrod54@gmail.com>
2023-08-17 12:41:00 -0400

    interview-prep-app - version 36.01 - NonTechnicalQuestions - Features: Starting Non Technical Questions

---
+++ src/components/non.technical.questions.component.jsx
@@ -0,0 +1,27 @@
+import React from "react";
+import { DataStructuresWrapper } from "../styled.components";
+
+/**interview-prep-app - version 36.01-  NonTechnicalQuestions - Features: 
+ * 
+ *     --> Starting 'Non Technical Questions' value
+ * 
+ * Note: this 'fasterFib' will invoke fibonacciMaster().
+ */
+
+const NonTechnicalQuestions = () => {
+
+    return(
+        <DataStructuresWrapper>
+            <h2>Non Technical Questions</h2>
+
+            <p>
+                when it comes to <strong> interview ( non technical )</strong>,
+                is good to set a <strong> mindest </strong> that every party 
+                involve in the interview is excited to be there ( )
+            </p>
+            
+        </DataStructuresWrapper>
+    )
+}
+
+export default NonTechnicalQuestions;
