0cde1168929819fc2a02968a20934c3f2866c2f6
Erick Rodriguez <erickrod54@gmail.com>
2023-08-18 03:11:07 -0400

    interview-prep-app - version 36.03 - NonTechnicalQuestions - Features: Setting the questions for Non Technical Questions

--- src/components/non.technical.questions.component.jsx
+++ src/components/non.technical.questions.component.jsx
@@ -1,27 +1,74 @@
 import React from "react";
 import { DataStructuresWrapper } from "../styled.components";
 
-/**interview-prep-app - version 36.01-  NonTechnicalQuestions - Features: 
- * 
- *     --> Starting 'Non Technical Questions' value
- * 
+/**interview-prep-app - version 36.03-  NonTechnicalQuestions - Features:
+ *
+ *     --> Setting the questions for 'Non Technical Questions' 
+ *
  * Note: this 'fasterFib' will invoke fibonacciMaster().
  */
 
 const NonTechnicalQuestions = () => {
+  return (
+    <DataStructuresWrapper>
+      <h2>Non Technical Questions</h2>
 
-    return(
-        <DataStructuresWrapper>
-            <h2>Non Technical Questions</h2>
+      <p>
+        when it comes to <strong> interview ( non technical )</strong>, is good
+        to set a <strong> mindest </strong> that every party involve in the
+        interview is excited to be there ( have the mindset that i am familiar
+        with the interviewer making him/her feel comfortable )
+      </p>
 
-            <p>
-                when it comes to <strong> interview ( non technical )</strong>,
-                is good to set a <strong> mindest </strong> that every party 
-                involve in the interview is excited to be there ( )
-            </p>
-            
-        </DataStructuresWrapper>
-    )
-}
+      <p>
+        always try to <strong> sense </strong> and <strong> match </strong> at
+        energy level
+      </p>
+
+      <p>there is a list of questions and common answers following:</p>
+
+      <ul>
+        <li>Can you do the job ?</li>
+
+        <p className="answer">
+
+        </p>
+
+        <li>Can i work with you ?</li>
+
+        <p className="answer"></p>
+
+        <li>Are you going to improve ?</li>
+
+        <p className="answer"></p>
+        <li>Tell me about a problem you solved ?</li>
+
+        <p className="answer"></p>
+
+        <li>Why do you want work with us ?</li>
+
+        <p className="answer"></p>
+        <li>What to ask to the interviewer ?</li>
+
+        <p className="answer"></p>
+        <li>Tell me about yourself ?</li>
+
+        <p className="answer"></p>
+
+        <li>What is your biggeest weakness ?</li>
+
+        <p className="answer"></p>
+
+        <li>Secret Weapon</li>
+
+        <p className="answer"></p>
+
+        <li>After the interview</li>
+
+        <p className="answer"></p>
+      </ul>
+    </DataStructuresWrapper>
+  );
+};
 
 export default NonTechnicalQuestions;
