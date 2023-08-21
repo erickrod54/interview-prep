a838231576f01d64c20e922ed125029354070284
Erick Rodriguez <erickrod54@gmail.com>
2023-08-21 12:58:17 -0400

    interview-prep-app - version 36.10 - OfferAndNegotiation - Features: Stating OfferAndNegotiation and answering topics

--- src/components/offer.and.negotiation.component.jsx
+++ src/components/offer.and.negotiation.component.jsx
@@ -1,10 +1,10 @@
 import React from "react";
 import { DataStructuresWrapper } from "../styled.components";
 
-/**interview-prep-app - version 36.09 - OfferAndNegotiation - 
+/**interview-prep-app - version 36.10 - OfferAndNegotiation - 
  * Features: 
  * 
- *     --> Building 'OfferAndNegotiation'  basic component.   
+ *     --> Stating 'OfferAndNegotiation'  and answering topics.   
  * 
  * Note: By this point i will start to relate algorithms with
  * data structures. the mergesorthalfagaindataset has been added
@@ -16,6 +16,59 @@ const OfferAndNegotiation = () => {
     return(
         <DataStructuresWrapper>
             <h2>Offer and Negotiation</h2>
+
+            <h3>Handling an offer:</h3>
+
+            <p>
+
+            </p>
+
+            <h3>Handling multiple offers:</h3>
+
+            <h3>Handling a rejection:</h3>
+
+            <p>
+                sometimes the greatest people of the tech get rejected ( this does not mean anything )
+                as dating, that does mean maybe was not the right fit at the moment, but 99 rejections
+                and 1 good offer is worth, cause the feedback of 99 rejections make me have that good
+                offer ( many of those examples at google )
+            </p>
+
+            <h3>Getting a raise:</h3>
+
+            <h3>Negotiation 101:</h3>
+
+            <p>
+                there is a lot of cultures and standards to make a negotiation, and multiple questions surge:
+
+                <ul>
+                    <li>
+                        What salary you desired ? ( R: the average software get around xxxxxx, we can start at that number )
+                    </li>
+                </ul>
+
+                and support this answer with some reasons to support the negotiation of this number, maybe involving a 
+                thirdparty as family or parthner in the decision making, so will be a way to support also the negiotiation 
+                process
+
+                <strong> cultures </strong> plays a big role in this process, there is some tips that can be include
+                in this process:
+
+                <ul>
+                    <li>
+                        Don't end the conversation
+                    </li>
+                    <li>
+                        Give reason for everything
+                    </li>
+                    <li>
+                        Always negotiate
+                    </li>
+                    <li>
+                        Be positive
+                    </li>
+                </ul>
+            </p>
 
         </DataStructuresWrapper>
     )
