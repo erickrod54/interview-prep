c20007701179b01e2cecf39f73fba7c5ff0a822f
Erick Rodriguez <erickrod54@gmail.com>
2023-08-20 23:01:15 -0400

    interview-prep-app - version 36.09 - OfferAndNegotiation - Features: Buiding OfferAndNegotiation basic Component

---
+++ src/components/offer.and.negotiation.component.jsx
@@ -0,0 +1,24 @@
+import React from "react";
+import { DataStructuresWrapper } from "../styled.components";
+
+/**interview-prep-app - version 36.09 - OfferAndNegotiation - 
+ * Features: 
+ * 
+ *     --> Building 'OfferAndNegotiation'  basic component.   
+ * 
+ * Note: By this point i will start to relate algorithms with
+ * data structures. the mergesorthalfagaindataset has been added
+ */
+
+
+const OfferAndNegotiation = () => {
+
+    return(
+        <DataStructuresWrapper>
+            <h2>Offer and Negotiation</h2>
+
+        </DataStructuresWrapper>
+    )
+}
+
+export default OfferAndNegotiation;
