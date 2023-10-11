7ff43524009d52d8abb8dbe5fd5d0a287cf79e44
Erick Rodriguez <erickrod54@gmail.com>
2023-10-10 21:30:00 -0400

    interview-prep-app - version 38.00 - context js - Features: Clearing setInsertionvalue

--- src/context.js
+++ src/context.js
@@ -1,10 +1,10 @@
 import React, { useContext, useState } from "react";
 import { array1, array2, array3, array4, array5, array6, array7, basket, baskets, beasts, boxes, boxes1, boxesletters, everyoneCharacter, graphsData, largeArray, letters, linksData, n, nemo, numbers, numberssearching, numberssorting, spanishs } from "./data";
 
-/**interview-prep-app - version 37.20 - context js - 
+/**interview-prep-app - version 38.00 - context js - 
  * Features: 
  * 
- *     --> Placing and providing 'insertionSort'
+ *     --> Clearing 'setInsertionsortvalue'
  * 
  * 
  * Note: pending to clear 'basketsortedfixed', 'setBasketsortedfixed'
@@ -779,7 +779,6 @@ export const AppProvider = ({ children }) => {
             bubblesortvalue,
             selectionsortvalue,
             insertionsortvalue, 
-            setInsertionsortvalue,
             insertionSort,
             handleSelectionSort,
             handleBubblesort,
